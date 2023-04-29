af616504e4d23f202392074f37e9f354d1370921
thesky <thesky341@foxmail.com>
2023-04-29 11:28:58 +0800

    添加.gitignore

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-docker run -v /Users/thesky/go/src/github.com/thesky341/my_stock_market:/go/src/github.com/thesky341/my_stock_market \
+docker run --rm -v /Users/thesky/go/src/github.com/thesky341/my_stock_market:/go/src/github.com/thesky341/my_stock_market \
   -t -i golang:latest  /bin/bash
